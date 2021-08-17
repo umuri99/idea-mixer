efde353231d424a7c7eb7d50b15b102dc27dc518
umuri99 <codingonly9900@gmail.com>
2021-08-18 08:14:44 +0900

    アカウントの有効化機能の追加

--- src/app/controllers/sessions_controller.rb
+++ src/app/controllers/sessions_controller.rb
@@ -7,9 +7,16 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
       #ユーザーログイン後にホームにリダイレクトする
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or root_path
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or root_path
+      else
+        message  = "アカウントが有効化されていません "
+        message += "メールにアカウント有効化用リンクが記載されてますので、ご確認ください"
+        flash[:danger] = message
+        redirect_to root_url
+      end
     else
       #エラーメッセージを作成する
       flash.now[:danger] = "メールアドレスまたはパスワードが違います"
