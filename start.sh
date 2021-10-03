f02c8284d0756921dc42ffa7d130d70430cce958
umuri99 <codingonly9900@gmail.com>
2021-10-03 10:58:30 +0900

    start.sh修正

--- start.sh
+++ start.sh
@@ -3,8 +3,6 @@
 if [ "${RAILS_ENV}" = "production" ]
 then
     bundle exec rails assets:precompile
-    bundle exec puma -C config/puma.rb
-    exit
 fi
 
 bundle exec rails s -p ${PORT:-3000} -b 0.0.0.0
